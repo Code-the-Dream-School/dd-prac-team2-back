9fc1ff3178a89b5a14a345da672f00c1c9cbe6b2
cdpaiva <cdpaivaa@gmail.com>
2023-08-21 10:12:45 -0400

    Returns only id and name for each cohort

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -4,15 +4,12 @@ import User from '../models/User';
 const getCohorts = async (req: Request, res: Response) => {
   const populateCohortOptions = {
     path: 'cohorts',
-    select: 'name weeks',
+    select: 'name',
   };
 
   const user = await User.findById(req.user.userId).populate(
     populateCohortOptions
   );
-
-  // if the user only have one cohort, we
-  // can directly present that cohort information in the home page
 
   return res.status(200).json({ cohorts: user!.cohorts });
 };
